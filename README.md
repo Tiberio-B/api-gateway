2b063348e22ffc977437812564f2d6a93e551f93
Tiberio <tiberio.b@hotmail.it>
2021-03-19 12:19:07 +0100

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+![plot](./z-uml/sequence-diagram.png)
---
+++ src/test/java/it/sogei/svildep/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sogei.svildep.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
